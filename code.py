5ac9ab758ba6f2129a2c109bad5fc84dcf61e89f
Cactys12 <201488348+Cactys12@users.noreply.github.com>
2026-02-15 19:58:42 +0000

    Updated the code
    Still broken, and I can't work our the problem

--- code.py
+++ code.py
@@ -22,11 +22,22 @@ modeltypes = {
     2: "2"
     3: "3"
     }
+timerlength = 3600
 
 while true:
+    display.fill(0)
     if current_type == 0 and button1 == true and button2 == false and button3 == false:
         current_type = 1
     if current_type == 0 and button1 == false and buttton2 == true and button3 == false:
         current_type = 2
     if current_type == 0 and button1 == false and buttton2 == false and button3 == true:
         currrent_type = 3
+    display.text(str(modeltypes[current_type]),0,0)
+    display.show()
+    if current_type == 1:
+        display.text("timer set for"+timerlength,0,0)
+        display.show()
+        timer.sleep(timerlength)
+        current_type = 0
+    if current_type == 2:
+        pass

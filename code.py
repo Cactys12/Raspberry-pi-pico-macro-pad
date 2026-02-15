e19fb61d98079efc9227b1b849d5c7d6f6194944
Cactys12 <201488348+Cactys12@users.noreply.github.com>
2026-02-15 20:09:19 +0000

    Adding the button code
    Forgot about the button code, but then I added it

--- code.py
+++ code.py
@@ -1,11 +1,17 @@
 import usb_hid
+import time
 import busio
+from machine import Pin
 from board import SCL, SDA
 from adafruit_hid.keyboard import Keyboard
 from adafruit_hid.keyboard_layout_us import KeyboardLayoutUS
 from adafruit_hid.mouse import Mouse
 from adafruit_hid.consumer_control import ConsumerControl
 from adafruit_hid.consumer_control_code import ConsumerControlCode
+
+button1 = Pin(13, Pin.IN, Pin.PULL_DOWN)
+button2 = Pin(8, Pin.IN, Pin.PULL_DOWN)
+button1 = Pin(3, Pin.IN, Pin.PULL_DOWN)
 
 kbd = Keyboard(usb_hid.devices)
 m = Mouse(usb_hid.devices)
@@ -26,11 +32,11 @@ timerlength = 3600
 
 while true:
     display.fill(0)
-    if current_type == 0 and button1 == true and button2 == false and button3 == false:
+    if current_type == 0 and button1.value() == true and button2.value() == false and button3.value() == false:
         current_type = 1
-    if current_type == 0 and button1 == false and buttton2 == true and button3 == false:
+    if current_type == 0 and button1.value() == false and buttton2.value() == true and button3.value() == false:
         current_type = 2
-    if current_type == 0 and button1 == false and buttton2 == false and button3 == true:
+    if current_type == 0 and button1.value() == false and buttton2.value() == false and button3.value() == true:
         currrent_type = 3
     display.text(str(modeltypes[current_type]),0,0)
     display.show()
@@ -39,5 +45,3 @@ while true:
         display.show()
         timer.sleep(timerlength)
         current_type = 0
-    if current_type == 2:
-        pass
